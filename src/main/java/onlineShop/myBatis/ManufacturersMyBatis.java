e94a2e90ac3733fe513fd18652101c1d3a4d46d9
AnastasiiaVariuchuk <stacyvariuchuk@gmail.com>
2022-09-26 00:48:52 +0300

    mapper Users, Manufacturers, Employees

--- src/main/java/onlineShop/myBatis/ManufacturersMyBatis.java
+++ src/main/java/onlineShop/myBatis/ManufacturersMyBatis.java
@@ -1,4 +1,50 @@
 package onlineShop.myBatis;
 
-public class ManufacturersMyBatis {
+
+import onlineShop.dao.idbcMySQLImpl.iproducts.IManufacturersDAO;
+import onlineShop.models.people.Users;
+import onlineShop.models.products.Manufacturers;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ManufacturersMyBatis implements IManufacturersDAO {
+    @Override
+    public Manufacturers getById(int id) {
+        SqlSession sqlSession = MyBatisConnection.getSqlSessionFactory().openSession();
+        Manufacturers manufacturers = sqlSession.selectOne("src.main.resources.myMappers.ManufacturersMapper.getBuId", id);
+        sqlSession.close();
+        return manufacturers;
+    }
+
+    @Override
+    public List<Manufacturers> getAll() {
+        List<Manufacturers> manufacturers= new LinkedList<>();
+        SqlSession sqlSession = MyBatisConnection.getSqlSessionFactory().openSession();
+        manufacturers = sqlSession.selectList("src.main.resources.myMappers.manufacturersMapper.getAll");
+        sqlSession.close();
+        return manufacturers;
+    }
+
+    @Override
+    public void add(Manufacturers manufacturers) {
+        SqlSession sqlSession = MyBatisConnection.getSqlSessionFactory().openSession();
+        sqlSession.insert("src.main.resources.myMappers.UsersMapper.add", manufacturers);
+        sqlSession.commit();sqlSession.close();
+    }
+
+    @Override
+    public void update(Manufacturers manufacturers) {
+        SqlSession sqlSession = MyBatisConnection.getSqlSessionFactory().openSession();
+        sqlSession.update("src.main.resources.myMappers.ManufacturersMapper.update", manufacturers.getIdManufacturer());
+        sqlSession.commit();sqlSession.close();
+    }
+
+    @Override
+    public void delete(int id) {
+        SqlSession sqlSession = MyBatisConnection.getSqlSessionFactory().openSession();
+        sqlSession.delete("src.main.resources.myMappers.ManufacturersMapper.delete", id);
+        sqlSession.commit();sqlSession.close();
+    }
 }
--- src/main/java/onlineShop/myBatis/UsersMyBatis.java
+++ src/main/java/onlineShop/myBatis/UsersMyBatis.java
@@ -1,4 +1,51 @@
 package onlineShop.myBatis;
 
-public class UsersMyBatis {
+import onlineShop.dao.idbcMySQL.people.UsersDAO;
+import onlineShop.dao.idbcMySQLImpl.ipeople.IUsersDAO;
+import onlineShop.models.people.Users;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class UsersMyBatis implements IUsersDAO {
+    @Override
+    public Users getById(int id) {
+        SqlSession sqlSession = MyBatisConnection.getSqlSessionFactory().openSession();
+        Users users = sqlSession.selectOne("src.main.resources.myMappers.UsersMapper.getBuId", id);
+        sqlSession.close();
+        return users;
+    }
+
+    @Override
+    public List<Users> getAll() {
+        List<Users> users = new LinkedList<>();
+        SqlSession sqlSession = MyBatisConnection.getSqlSessionFactory().openSession();
+        users = sqlSession.selectList("src.main.resources.myMappers.UsersMapper.getAll");
+        sqlSession.close();
+        return users;
+    }
+
+    @Override
+    public void add(Users users) {
+        SqlSession sqlSession = MyBatisConnection.getSqlSessionFactory().openSession();
+        sqlSession.insert("src.main.resources.myMappers.UsersMapper.add", users);
+        sqlSession.commit();sqlSession.close();
+    }
+
+    @Override
+    public void update(Users users) {
+        SqlSession sqlSession = MyBatisConnection.getSqlSessionFactory().openSession();
+        sqlSession.update("src.main.resources.myMappers.UsersMapper.update", users.getIdUser());
+        sqlSession.commit();sqlSession.close();
+    }
+
+    @Override
+    public void delete(int id) {
+        SqlSession sqlSession = MyBatisConnection.getSqlSessionFactory().openSession();
+        sqlSession.delete("src.main.resources.myMappers.UsersMapper.delete", id);
+        sqlSession.commit();sqlSession.close();
+    }
 }
